1cab7a212172fffc5e336cebb2aa6592fa0e76cf
amending1 <joasia.sowiak@gmail.com>
2023-12-13 11:42:03 +0100

    update index.js
    problem z pobieraniem danych z serwera

--- src/index.js
+++ src/index.js
@@ -6,52 +6,74 @@ import { searchImages } from 'pixabay-api';
 
 const form = document.getElementById('search-form');
 const gallery = document.getElementById('gallery');
-const loadMoreBtn = document.getElementById('load-more');
+const loadMoreBtn = document.querySelector('.load-more');
 let currentPage = 1;
 let totalHits = 0;
 
 form.addEventListener('submit', async function (event) {
   event.preventDefault();
-  const searchQuery = event.target.searchQuery.value.trim();
+  const searchQuery = event.target.searchQuery.value.trim(); // to co użytkownik wpisał w pole wyszukiwania (bez białych )
 
   if (!searchQuery) {
     Notiflix.Notify.failure('Please enter a search query.');
     return;
-  }
+  } // jeśli pole formularza jest puste użytkownik dostanie komunikat o tym
 
-  // Clear gallery and reset pagination when a new search is performed
+  // w przypadku nowego wyszukiwania usuwam zawartość elementu 'gallery', bo chcę żeby nowe wyniki były wyświetlane od nowa, bez zawartości poprzedniego wyszukiwania
+  //i resetuję zmienną 'currentPage'
   gallery.innerHTML = '';
   currentPage = 1;
 
+  // funkcja 'searchImages' wysyła żądanie do serwera Pixabay , aby uzyskać wyniki wyszukiwania dla określonego zapytania i numeru strony
+  // funkcja jest asynchroniczna, więc wykonanie reszty kodu zostanie wstrzymane do momentu uzyskania odpowiedzi z funkcji 'searchImages'. Oczywiście ta odpowiedź będzie zawierać dane - obiekty obrazków
+  // funkcja 'handleSearchResults' odpowiada za obsługę otrzymanych danych (generowanie kart obrazków, aktualizację interfejsu)
   const data = await searchImages(searchQuery, currentPage);
   handleSearchResults(data);
 });
 
+// obsługa przycisku 'Load more'
 loadMoreBtn.addEventListener('click', async function () {
+  //zwiększam wartość o 1 (kiedy użytkownik kliknie przycisk, chcę pobrać kolejną stronę wynikow)
   currentPage++;
+
+  // chcę użyć tego samego zapytania wyszukiwania, które zostało wprowadzone wcześniej
   const searchQuery = form.searchQuery.value.trim();
+
+  // wywołuję funkcję 'searchImages' z NOWYM numerem strony
   const data = await searchImages(searchQuery, currentPage);
   handleSearchResults(data);
 });
 
+// tworzę żądania HTTP do serwera Pixabay
 async function searchImages(query, page) {
   const apiKey = '41213860-1b24a2642cc998ee0211e05b1';
   const perPage = 40;
+
+  // tworzę pełny adres URL żądania wysyłanego do serwera za pomocą funkcji 'fetch'
   const apiUrl = `https://pixabay.com/api/?key=${apiKey}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`;
 
   try {
+    // funkcja fetch jest asynchroniczna i zatrzymuje wykonywanie kolejnych linii kodu do momentu zakończenia żądania. Odpowiedź z serwera jest przechowywana w zmiennej response
     const response = await fetch(apiUrl);
+
+    // wywołuję metodę '.json()' na obiekcie 'response', żeby przekonwertować otrzymaną odpowiedź do formaty JSON
     const data = await response.json();
+
+    //następuje zwrócenie danych z funkcji 'searchImages'
     return data;
   } catch (error) {
     console.error('Error fetching images:', error);
+
+    // w przypadku błedu funkcja zwróci pustą tablicę obiektów obrazków
     return { hits: [] };
   }
 }
 
 function handleSearchResults(data) {
-  const { hits, totalHits: newTotalHits } = data;
+  // robię destrukturyzację obiektu 'data', żeby mieć dostęp do właściwosci 'hits' (czyli tablicy obrazków) i 'totalHits' (czyliłącznej liczby obrazków dostępnych dla danego zapytania)
+  const { hits, totalHits } = data;
 
+  // Sprawdzam, czy tablica 'hits' jest pusta (nie zawiera żadnych obrazków). Jeśli tak, informuję użytkownika, że nie znaleziono obrazków spełniających kryteria wyszukiwania. Ukrywam też przycisk "Load more", bo nie ma więcej obrazków do wczytania.
   if (hits.length === 0) {
     Notiflix.Notify.info(
       'Sorry, there are no images matching your search query. Please try again.'
@@ -60,24 +82,30 @@ function handleSearchResults(data) {
     return;
   }
 
-  totalHits = newTotalHits;
-
+  //Sprawdzam, czy obecnie jesteśmy na pierwszej stronie wyników
   if (currentPage === 1) {
     Notiflix.Notify.success(`Hooray! We found ${totalHits} images.`);
   }
 
-  loadMoreBtn.style.display = hits.length < totalHits ? 'block' : 'none';
+  //Ustawiam widoczność przycisku "Load more" na podstawie warunku, czy liczba obrazków na stronie jest mniejszee niz łączna liczba obrazków
+  if (hits.length < totalHits) {
+    loadMoreBtn.style.display = 'block';
+  } else {
+    loadMoreBtn.style.display = 'none';
+  }
 
-  const fragment = document.createDocumentFragment();
+  // tworzę tablicę, a potem dla każdego elementu w 'hits' (tablica stworzona w funkcji 'searchImages') jest tworzony kafelek 'card' i dodawany do tej tablicy
+  const cards = [];
 
   hits.forEach(hit => {
     const card = createPhotoCard(hit);
-    fragment.appendChild(card);
+    cards.push(card);
   });
 
-  gallery.appendChild(fragment);
+  // używam operatora spread (...) do dodania wszystkich elementów z tablicy cards do galerii
+  gallery.append(...cards);
 
-  // Smooth scroll to the newly loaded images
+  //  płynne przewijanie strony po wywołaniu żądania i przy renderowaniu się każdej następnej grupy obrazków (kod skopiowany z treści zadania)
   const { height: cardHeight } = document
     .querySelector('.gallery')
     .firstElementChild.getBoundingClientRect();
@@ -88,7 +116,9 @@ function handleSearchResults(data) {
   });
 }
 
+// tworzę element kafelka obrazka 'photo-card'
 function createPhotoCard(hit) {
+  // destrukturyzacja danych obrazka
   const {
     webformatURL,
     largeImageURL,
@@ -99,17 +129,22 @@ function createPhotoCard(hit) {
     downloads,
   } = hit;
 
+  // tworzę element 'div' dla kafelka
   const card = document.createElement('div');
   card.className = 'photo-card';
 
+  // tworzę element 'img' dla obrazka i ustawiam dla niego atrybuty
   const img = document.createElement('img');
   img.src = webformatURL;
   img.alt = tags;
   img.loading = 'lazy';
 
+  // tworzę nowy element 'div' o klasie 'info', który będzie zawierał dodatkowe informacj o obrazku
+
   const info = document.createElement('div');
   info.className = 'info';
 
+  // tworzę elementy 'p' dla poszczególnych informacji i dodaję je do div 'info'
   const infoItems = [
     `<p class="info-item"><b>Likes:</b> ${likes}</p>`,
     `<p class="info-item"><b>Views:</b> ${views}</p>`,
@@ -119,6 +154,7 @@ function createPhotoCard(hit) {
 
   info.innerHTML = infoItems.join('');
 
+  // dodaję elementy 'img' i 'info' do kafelka 'card'
   card.appendChild(img);
   card.appendChild(info);
 
